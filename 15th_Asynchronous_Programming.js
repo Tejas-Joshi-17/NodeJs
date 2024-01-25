54e08fd9eecd8ca5736de68b8cb653123b2f6b56
Tejas-Joshi-17 <joshitejas188@gmail.com>
2024-01-25 15:40:17 +0530

    Add 15th_Asynchronous_Programming File

---
+++ 15th_Asynchronous_Programming.js
@@ -0,0 +1,12 @@
+
+console.log(`Program Start`)
+
+setTimeout(() =>{
+    console.log(`Tejas Dattatray Joshi`)
+}, 2000)
+
+console.log(`Program End`)
+
+// Program Start
+// Program End
+// Tejas Dattatray Joshi        <------ After 2 Seconds
